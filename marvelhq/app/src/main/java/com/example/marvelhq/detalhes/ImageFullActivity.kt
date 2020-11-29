97ac3f672817783a87ff7208aa0489d485768152
Sara Alves <saraalves14@hotmail.com>
2020-11-29 19:58:00 -0300

    tela de detalhes

---
+++ marvelhq/app/src/main/java/com/example/marvelhq/detalhes/ImageFullActivity.kt
@@ -0,0 +1,12 @@
+package com.example.marvelhq.detalhes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.marvelhq.R
+
+class ImageFullActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_full)
+    }
+}
